c3e983bc85b19c413c4836b11f03fd2b40e8a69c
k4r1nka <karin.ciznarova@gmail.com>
2025-05-20 17:00:11 +0200

    :)

---
+++ web.py
@@ -0,0 +1,93 @@
+
+from flask import Flask, request, render_template
+from flask_sqlalchemy import SQLAlchemy
+import os 
+import sqlite3
+
+app = Flask(__name__, instance_relative_config=True)
+
+db_path = os.path.join(app.instance_path, "kurzy.db")
+
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{db_path}".replace("\\", "/")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+class Kurz(db.Model):
+    __tablename__ = "Kurzy"
+    ID_Kurzu         = db.Column(db.Integer, primary_key = True )
+    Nazov_kurzu      = db.Column(db.String)
+    Typ_sportu       = db.Column(db.String)
+    Max_pocet_ucastnikov  = db.Column(db.Integer )
+    ID_trenera       = db.Column(db.Integer )
+
+    def __repr__(self):
+        return f"<kurz {self.Nazov_kurzu}>"
+    
+class Trener(db.Model):
+    __tablename__ = "Treneri"
+    ID_trenera    = db.Column(db.Integer, primary_key = True )
+    Meno          = db.Column(db.String)
+    Priezvisko    =  db.Column(db.String)
+    Specializacia =  db.Column(db.String)
+    Telefon       = db.Column(db.String)
+
+    def __repr__(self):
+        return f"<trener {self.ID_trenera}>"
+    
+class Miesto(db.Model):
+    __tablename__ = "Miesta"
+    ID_miesta     = db.Column(db.Integer, primary_key = True )
+    Nazov_miesta  = db.Column(db.String)
+    Adresa        = db.Column(db.String)
+    Kapacita      = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f"<miesto {self.ID_miesta}>"
+
+class Maxkap(db.Model):
+    __tablename__         = "Maxkap"
+    Max_pocet_ucastnikov  = db.Column(db.Integer)
+    ID_kurzu              = db.Column(db.Integer, primary_key = True)
+
+    def __repr__(self):
+        return f"<maxkap {self.ID_kurzu}>"
+
+
+def data():
+    conn = sqlite3.connect("kurzy.db")
+    return conn
+
+@app.route('/')  
+def index():
+    return render_template("home.html")
+        
+
+
+@app.route('/kurzy')  
+def zobraz_kurzy():
+    kurzy = Kurz.query.all()
+    return render_template("kurzy.html", kurzy=kurzy)
+
+
+@app.route('/treneri')  
+def zobraz_trenerov():
+    treneri = Trener.query.all()
+    return render_template("treneri.html",treneri = treneri)
+
+
+@app.route('/miesta')
+def miesta():
+    miesta = Miesto.query.all()
+    return render_template("miesta.html",miesta = miesta)
+
+
+
+@app.route('/max_kap')
+def kapacita():
+    kapacita = Maxkap.query.all()
+    return render_template("maxkap.html",max_kap = kapacita)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
